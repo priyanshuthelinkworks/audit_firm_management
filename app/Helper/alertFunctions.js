9aff851e05e3846f1a4886015847d42d0e1e9db3
priyanshu shakya <priyanshu@thelinkworks.com>
2024-12-24 17:59:17 +0530

    change

--- app/Helper/alertFunctions.js
+++ app/Helper/alertFunctions.js
@@ -151,7 +151,7 @@ class Alert {
   static sendSlackMessage = async (message, slackid) => {
 
     try {
-      // const token = 'xoxb-7676865936496-7640706705591-tPnlOaSyXXWXJmIq1AjmK9eK';
+      const token = '';
 
       const web = new WebClient(token);
 
